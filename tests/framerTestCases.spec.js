173a90f8b15a28d5540891e9b28845b8670bae7b
MG0007 <103684005+MG0007@users.noreply.github.com>
2024-12-01 19:00:37 +0200

    Confirm color for Sign up button in Components page

--- tests/framerTestCases.spec.js
+++ tests/framerTestCases.spec.js
@@ -27,3 +27,31 @@ test("Does contain 4 questions in the FAQ section from 'Pricing' page", async ({
     const allQuestionsCount = await page.locator(locators.pricingPageQuestions).count();
     expect(allQuestionsCount).toEqual(locators.fqaNumberOfQuestions);
 })
+
+test("Does display 'visit' button on each card in the 'Many types of components to customize' section", async ({ page }) => {
+    await page.goto(locators.homePage)
+    await page.locator(locators.componentsPage).click();
+
+    const countCardVisitButton = await page.locator(locators.componentsPageCardsVisitButtons).count();
+    const cardCount = await page.locator(locators.componentsPageCard).count();
+    const cardSmallCount = await page.locator(locators.componentsPageCardSmall).count();
+
+    expect(cardCount + cardSmallCount).toEqual(countCardVisitButton);
+})
+
+test("Does display 'Sign up' button's color as 'rgb(255,82,79'", async ({ page }) => {
+    await page.goto(locators.homePage);
+    await page.locator(locators.componentsPage).click();
+
+    await page.locator(locators.footerSection).scrollIntoViewIfNeeded();
+
+    const attributes = await page.locator(locators.footerSignUpButton).evaluate((el) => getComputedStyle(el).background);
+
+    const attrSplit = attributes.split(' ');
+
+    const buttonColor = attrSplit[0] + attrSplit[1] + attrSplit[2];
+
+    await expect(page.locator(locators.footerSubscribeSection)
+        .locator(locators.footerSignUpButton)).toHaveText('Sign Up');
+    expect(buttonColor).toBe(locators.componentsPageSignUpButton);
+})
