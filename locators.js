89b2a38b98fa4d2d95610dd3d583d15994d9d916
MG0007 <103684005+MG0007@users.noreply.github.com>
2024-12-01 11:37:32 +0200

    Test home page loads successfully and click on Pricing in the main menu leads to Pricing page

---
+++ locators.js
@@ -0,0 +1,7 @@
+const locators ={
+    homePage: 'https://mind-wend-913065.framer.app/',
+    pricingPage: 'div.framer-1g3amks-container div[data-framer-generated="true"] span',
+    pricingPageTitle: 'div.framer-1xy1tag h1 span',
+}
+
+export{locators}
