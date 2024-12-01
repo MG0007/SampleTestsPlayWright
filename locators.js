2221126dcecba6254e1944925a70b0a756bf57dd
MG0007 <103684005+MG0007@users.noreply.github.com>
2024-12-01 13:22:14 +0200

    Verifyes all products have $ in front of the price

--- locators.js
+++ locators.js
@@ -2,6 +2,8 @@ const locators ={
     homePage: 'https://mind-wend-913065.framer.app/',
     pricingPage: 'div.framer-1g3amks-container div[data-framer-generated="true"] span',
     pricingPageTitle: 'div.framer-1xy1tag h1 span',
+    pricingPageTiles: 'div.pricing-container h2 span',
+    
 }
 
 export{locators}
--- tests/framerTestCases.spec.js
+++ tests/framerTestCases.spec.js
@@ -6,3 +6,14 @@ test("Visit Framer home page and click 'Pricing'", async ({ page }) => {
     await page.locator(locators.pricingPage).click();
     await expect(page.locator(locators.pricingPageTitle)).toHaveText('Pricing');
 })
+
+test("Does display '$' for products in Pricing page", async({page})=>{
+    await page.goto(locators.homePage);
+    await page.locator(locators.pricingPage).click();
+
+    const priceTagAllTiles = await page.locator(locators.pricingPageTiles).allTextContents();
+
+    priceTagAllTiles.forEach((priceTag)=>{
+        expect(priceTag[0]).toEqual('$');
+    })
+})
