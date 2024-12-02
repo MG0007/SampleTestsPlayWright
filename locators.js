070571fc2a707fef0d5d8ff4e9c55c839b4792bf
MG0007 <103684005+MG0007@users.noreply.github.com>
2024-12-02 14:40:53 +0200

    Reorganized locators

--- locators.js
+++ locators.js
@@ -1,24 +1,37 @@
-const locators ={
-    homePage: 'https://mind-wend-913065.framer.app/',
-    pricingPage: 'div.framer-1g3amks-container div[data-framer-generated="true"] span',
-    pricingPageTitle: 'div.framer-1xy1tag h1 span',
-    pricingPageTiles: 'div.pricing-container h2 span',
-    pricingPageQuestions: 'div.framer-cx0lih [values="[object Object]"]',
-    fqaNumberOfQuestions: 4,
-    componentsPage: 'div.framer-15kr31b-container span',
-    componentsPageCardsVisitButtons: 'div.framer-1ytqfhu div[values="[object Object]"] div[name="Button"]',
-    componentsPageCard: 'div.framer-1ytqfhu div[values="[object Object]"] div[name="Card"]',
-    componentsPageCardSmall: 'div.framer-1ytqfhu div[values="[object Object]"] div[name="Card Small"]',
-    footerSection: 'div.framer-1jtqeum',
-    footerSubscribeSection: 'div.framer-l3ry8h',
-    footerSignUpButton: 'form input[value="Sign Up"]',
-    componentsPageSignUpButton: 'rgb(255,82,79)',
-    homePageGetTheAppButton: 'div.framer-14g223s span',
-    updatesPage: 'div.framer-7hrpx3-container span',
-    updatesPageTitle: 'div.framer-1oe4f0f h1 span',
-    homePageMobileAppVideoButtons: 'div.framer-15y7aei > div',
-    homePage3DButton: 'div.framer-yvxyy1 span',
-    animation3D: 'div.framer-xattrb[data-framer-name="Variant 2"]',
+const locators = {
+    homePage: {
+        homePageUrl: 'https://mind-wend-913065.framer.app/',
+        homePageGetTheAppButton: 'div.framer-14g223s span',
+        homePageGetTheAppButton: 'div.framer-14g223s span',
+        homePageMobileAppVideoButtons: 'div.framer-15y7aei > div',
+        homePage3DButton: 'div.framer-yvxyy1 span',
+        animation3D: 'div.framer-xattrb[data-framer-name="Variant 2"]',
+    },
+    pricingPage: {
+        pricingPageUrl: 'div.framer-1g3amks-container div[data-framer-generated="true"] span',
+        pricingPageTitle: 'div.framer-1xy1tag h1 span',
+        pricingPageTiles: 'div.pricing-container h2 span',
+        pricingPageQuestions: 'div.framer-cx0lih [values="[object Object]"]',
+        fqaNumberOfQuestions: 4,
+    },
+    componentsPage: {
+        componentsPageUrl: 'div.framer-15kr31b-container span',
+        componentsPageCardsVisitButtons: 'div.framer-1ytqfhu div[values="[object Object]"] div[name="Button"]',
+        componentsPageCard: 'div.framer-1ytqfhu div[values="[object Object]"] div[name="Card"]',
+        componentsPageCardSmall: 'div.framer-1ytqfhu div[values="[object Object]"] div[name="Card Small"]',
+        componentsPageSignUpButton: 'rgb(255,82,79)',
+
+    },
+    footerSection: {
+        footerSec: 'div.framer-1jtqeum',
+        footerSubscribeSection: 'div.framer-l3ry8h',
+        footerSignUpButton: 'form input[value="Sign Up"]',
+    },
+
+    updatesPage: {
+        updatesPageUrl: 'div.framer-7hrpx3-container span',
+        updatesPageTitle: 'div.framer-1oe4f0f h1 span',
+    },
 }
 
-export{locators}
+export { locators }
