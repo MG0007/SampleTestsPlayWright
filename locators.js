94b955d18b825f5bb651a51a1d1120925b0f4fb3
MG0007 <mgdreamspersonalgithub@gmail.com>
2025-01-10 20:50:06 +0200

    Variable naming optimization

--- locators.js
+++ locators.js
@@ -1,39 +1,35 @@
 const locators = {
     homePage: {
-        homePageUrl: 'https://mind-wend-913065.framer.app/',
-        homePageGetTheAppButton: 'div.framer-14g223s span',
-        homePageGetTheAppButton: 'div.framer-14g223s span',
-        homePageMobileAppVideoButtons: 'div.framer-15y7aei > div',
-        homePage3DButton: 'div.framer-yvxyy1 span',
+        PageUrl: 'https://mind-wend-913065.framer.app/',
+        PageGetTheAppButton: 'div.framer-14g223s span',
+        PageMobileAppVideoButtons: 'div.framer-15y7aei > div',
+        Page3DButton: 'div.framer-yvxyy1 span',
         animation3D: 'div.framer-xattrb[data-framer-name="Variant 2"]',
     },
     pricingPage: {
-        pricingPageUrl: 'div.framer-1g3amks-container div[data-framer-generated="true"] span',
-        pricingPageTitle: 'div.framer-1xy1tag h1 span',
-        pricingPageTiles: 'div.pricing-container h2 span',
-        pricingPageQuestions: 'div.framer-cx0lih [values="[object Object]"]',
+        PageUrl: 'div.framer-1g3amks-container div[data-framer-generated="true"] span',
+        PageTitle: 'div.framer-1xy1tag h1 span',
+        PageTitles: 'div.pricing-container h2 span',
+        PageQuestions: 'div.framer-cx0lih [values="[object Object]"]',
         fqaNumberOfQuestions: 4,
     },
     componentsPage: {
-        componentsPageUrl: 'div.framer-15kr31b-container span',
-        componentsPageCardsVisitButtons: 'div.framer-1ytqfhu div[values="[object Object]"] div[name="Button"]',
-        componentsPageCard: 'div.framer-1ytqfhu div[values="[object Object]"] div[name="Card"]',
-        componentsPageCardSmall: 'div.framer-1ytqfhu div[values="[object Object]"] div[name="Card Small"]',
-        componentsPageSignUpButton: 'rgb(255,82,79)',
+        PageUrl: 'div.framer-15kr31b-container span',
+        PageSignUpButton: 'rgb(255,82,79)',
         largeCard: 'div.framer-1ytqfhu div[name="Card"]',
         smallCard: 'div.framer-1ytqfhu div[name="Card Small"]',
         cardAddition: ' div[name="Button"] p span.framer-text',
         buttonText: 'Visit'
     },
     footerSection: {
-        footerSec: 'div.framer-1jtqeum',
-        footerSubscribeSection: 'div.framer-l3ry8h',
-        footerSignUpButton: 'form input[value="Sign Up"]',
+        Sec: 'div.framer-1jtqeum',
+        SubscribeSection: 'div.framer-l3ry8h',
+        SignUpButton: 'form input[value="Sign Up"]',
     },
 
     updatesPage: {
-        updatesPageUrl: 'div.framer-7hrpx3-container span',
-        updatesPageTitle: 'div.framer-1oe4f0f h1 span',
+        PageUrl: 'div.framer-7hrpx3-container span',
+        PageTitle: 'div.framer-1oe4f0f h1 span',
     },
 }
 
